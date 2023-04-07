8f4c20cb68eeb60fd45b4aec23e6cdb3009e64f3
dborodin836 <dborodin836@gmail.com>
2023-04-07 22:47:53 +0300

    FEAT: added simple gui

---
+++ gui/log_window.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+
+from utils.chat import handle_gui_console_commands
+
+
+class LogWindow(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.log_text = None
+        self.cmd_line = None
+        self.create_widgets()
+        self.master.title("TF2-GPTChatBot")
+        self.master.iconbitmap("icon.ico")
+
+    def create_widgets(self):
+        # Add a Text widget to the window for displaying logs
+        self.log_text = tk.Text(self, height=20, width=100)
+        self.log_text.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+        # Add another Text widget below the log_text widget for displaying additional text
+        self.cmd_line = tk.Text(self, height=1, width=100)
+        self.cmd_line.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+        self.cmd_line.bind("<Return>", self.handle_commands)
+
+        # Add a placeholder to the additional_text widget
+        self.cmd_line.insert("1.0", "Type your commands here...")
+
+        # Binds to make the placeholder work
+        self.cmd_line.bind("<FocusIn>", self.handle_additional_text_focus_in)
+        self.cmd_line.bind("<FocusOut>", self.handle_additional_text_focus_out)
+
+    def update_logs(self, message):
+        self.log_text.insert(tk.END, f"{message}")
+
+    def exit_program(self):
+        self.master.destroy()
+
+    def handle_commands(self, event):
+        text = self.cmd_line.get("1.0", tk.END).strip()
+
+        handle_gui_console_commands(text)
+
+        # Clear the additional_text widget after the function is executed
+        self.cmd_line.delete("1.0", tk.END)
+
+    def handle_additional_text_focus_in(self, event):
+        # Clear the placeholder text when the additional_text widget receives focus
+        if self.cmd_line.get("1.0", tk.END).strip() == "Type your commands here...":
+            self.cmd_line.delete("1.0", tk.END)
+
+    def handle_additional_text_focus_out(self, event):
+        # Show the placeholder text when the additional_text widget loses focus and is empty
+        if not self.cmd_line.get("1.0", tk.END).strip():
+            self.cmd_line.insert("1.0", "Type your commands here...")
+
+
+class CustomOutput:
+    def __init__(self, window: LogWindow):
+        self.window = window
+
+    def write(self, message):
+        self.window.update_logs(message)
+
+    def flush(self):
+        ...
--- utils/chat.py
+++ utils/chat.py
@@ -1,8 +1,10 @@
 from typing import Literal
 
+from config import GPT_COMMAND, CHATGPT_COMMAND, CLEAR_CHAT_COMMAND
 from services.chatgpt import send_gpt_completion_request
-from services.network import send_say_command_to_tf2
-from utils.text import add_prompts_by_flags
+from services.network import send_say_command_to_tf2, check_connection
+from utils.prompt import load_prompts
+from utils.text import add_prompts_by_flags, open_tf2_logfile
 from utils.logs import log_message
 
 
@@ -29,3 +31,35 @@ def handle_gpt_request(message_type: Literal["CHAT", "GPT3"], username: str, use
 
     send_say_command_to_tf2(response)
     return chat_buffer
+
+
+def parse_tf2_console() -> None:
+    # Initialize conversation history
+    conversation_history = ''
+
+    check_connection()
+
+    # Load prompts from .xtx files
+    load_prompts()
+    print("Ready to use!")
+
+    # Loop through log file
+    for line, user in open_tf2_logfile():
+        # Handle GPT3 completion requests
+        if line.strip().startswith(GPT_COMMAND):
+            prompt = line.removeprefix(GPT_COMMAND).strip()
+            conversation_history = handle_gpt_request("GPT3", user, prompt, conversation_history)
+
+        # Handle ChatGPT requests
+        if line.strip().startswith(CHATGPT_COMMAND):
+            prompt = line.removeprefix(CHATGPT_COMMAND).strip()
+            conversation_history = handle_gpt_request("CHAT", user, prompt, conversation_history)
+
+        # Handle clear chat requests
+        if line.strip().startswith(CLEAR_CHAT_COMMAND):
+            log_message("CHAT", user, "CLEARING CHAT")
+            conversation_history = ''
+
+
+def handle_gui_console_commands(command):
+    print(command)
