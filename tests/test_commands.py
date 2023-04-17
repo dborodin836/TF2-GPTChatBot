3eb612cab42e2fb2616f322612056d816c9931ff
dborodin836 <dborodin836@gmail.com>
2023-04-17 20:01:50 +0300

    WIP: added tests for commands

---
+++ tests/test_commands.py
@@ -0,0 +1,31 @@
+from utils.commands import get_bot_state, start_bot, stop_bot, print_help_command
+
+
+def test_print_help_command(capsys):
+    print_help_command()
+    captured = capsys.readouterr()
+    assert "start - start the bot" in captured.out
+    assert "stop - stop the bot" in captured.out
+    assert "quit - quit the program" in captured.out
+    assert "bans - show all banned players" in captured.out
+    assert "ban <username> - ban user by username" in captured.out
+    assert "unban <username> - unban user by username" in captured.out
+    assert "gpt3 <prompt> - sends a response to GPT3" in captured.out
+
+
+def test_start_bot():
+    start_bot()
+    assert get_bot_state() is True
+
+
+def test_stop_bot():
+    stop_bot()
+    assert get_bot_state() is False
+
+
+def test_get_bot_state():
+    start_bot()
+    assert get_bot_state() is True
+
+    stop_bot()
+    assert get_bot_state() is False
