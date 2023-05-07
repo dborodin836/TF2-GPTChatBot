7024c3168fedaf812d3f1bf3a1b3d792ea45ef2e
dborodin836 <dborodin836@gmail.com>
2023-05-07 22:39:36 +0300

    FEAT: added hotkey to switch bot's state (hardcoded as F11)

--- utils/bot_state.py
+++ utils/bot_state.py
@@ -1,6 +1,9 @@
+import keyboard
+
 from utils.logs import log_cmd_message
 
 _BOT_RUNNING = True
+SWITCH_STATE_HOTKEY = "F11"
 
 
 def start_bot() -> None:
@@ -24,3 +27,9 @@ def switch_state() -> None:
         stop_bot()
     else:
         start_bot()
+
+
+def switch_state_hotkey_handler() -> None:
+    while True:
+        keyboard.wait(SWITCH_STATE_HOTKEY)
+        switch_state()
