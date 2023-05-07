d8f0fa22694f11fc2b82ea19ba3eca6fad1cb557
dborodin836 <dborodin836@gmail.com>
2023-05-07 22:53:49 +0300

    REFACTOR: moved some functions from chat.py to github.py & io_buffer.py, added docstrings

--- utils/chat.py
+++ utils/chat.py
@@ -1,13 +1,9 @@
-import os
 import queue
 import time
 
-import requests
-import json
-from io import StringIO
-
-from config import config, BUFFERED_CONFIG_INIT_LOG_MESSAGES
+from config import config
 from services.chatgpt import handle_gpt_request
+from services.github import check_for_updates
 from services.source_game import check_connection, send_say_command_to_tf2, get_username
 from utils.bans import unban_player, ban_player, load_banned_players, is_banned_username
 from utils.commands import (handle_rtd_command, stop_bot, start_bot, get_bot_state,
@@ -19,26 +15,19 @@ from utils.logs import log_message
 PROMPTS_QUEUE = queue.Queue()
 
 
-def check_for_updates():
-    api_url = 'https://api.github.com/repos/dborodin836/TF2-GPTChatBot/releases/latest'
-    response = requests.get(api_url)
-
-    data = json.loads(response.content)
-    latest_version = data['tag_name']
-
-    if latest_version > config.APP_VERSION:
-        print(f'A new version ({latest_version}) of the app is available. Please update.')
-    else:
-        print(f'The app is up to date. ({latest_version})')
-
-
-def set_host_username():
+def set_host_username() -> None:
+    """
+    Sets the username in config.
+    """
     username = get_username()
     config.HOST_USERNAME = username
     print(f"Hello '{config.HOST_USERNAME}'!")
 
 
-def setup():
+def setup() -> None:
+    """
+    Initializes the program.
+    """
     print("""
       _____ _____ ____        ____ ____ _____ ____ _           _   ____        _   
      |_   _|  ___|___ \      / ___|  _ \_   _/ ___| |__   __ _| |_| __ )  ___ | |_ 
@@ -55,16 +44,6 @@ def setup():
     load_banned_players()
 
 
-def print_buffered_config_innit_messages():
-    BUFFERED_CONFIG_INIT_LOG_MESSAGES.seek(0)
-
-    if get_io_string_size(BUFFERED_CONFIG_INIT_LOG_MESSAGES) > 0:
-        for line in BUFFERED_CONFIG_INIT_LOG_MESSAGES:
-            print(line, end='')
-    else:
-        print("Ready to use!")
-
-
 def parse_tf2_console_logs() -> None:
     conversation_history: str = ''
 
@@ -76,13 +55,6 @@ def parse_tf2_console_logs() -> None:
         if is_banned_username(logline.username):
             continue
         conversation_history = handle_command(logline, conversation_history)
-
-
-def get_io_string_size(string_io: StringIO):
-    string_io.seek(0, os.SEEK_END)
-    length = string_io.tell()
-    string_io.seek(0)
-    return length
 
 
 def handle_command(logline: LogLine, conversation_history: str) -> str:
---
+++ utils/io_buffer.py
@@ -0,0 +1,27 @@
+import os
+from io import StringIO
+
+from config import BUFFERED_CONFIG_INIT_LOG_MESSAGES
+
+
+def get_io_string_size(string_io: StringIO) -> int:
+    """
+    Calculate the number of string stored within a StringIO object.
+    """
+    string_io.seek(0, os.SEEK_END)
+    length = string_io.tell()
+    string_io.seek(0)
+    return length
+
+
+def print_buffered_config_innit_messages() -> None:
+    """
+    Prints the initialization messages saved in the BUFFERED_CONFIG_INIT_LOG_MESSAGES buffer.
+    """
+    BUFFERED_CONFIG_INIT_LOG_MESSAGES.seek(0)
+
+    if get_io_string_size(BUFFERED_CONFIG_INIT_LOG_MESSAGES) > 0:
+        for line in BUFFERED_CONFIG_INIT_LOG_MESSAGES:
+            print(line, end='')
+    else:
+        print("Ready to use!")
