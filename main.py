c7c63806017fe802d02fd09b3f887d5d5dfdb479
dborodin836 <dborodin836@gmail.com>
2023-05-31 16:05:50 +0300

    FIX: added error suppression in status command sender

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-from utils.bot_state import switch_state_hotkey_handler
 from config import init_config
 # This is required due to config used in imported modules
 init_config()
 
+import time
 import sys
 import tkinter as tk
 import threading
+import contextlib
 
 from gui.log_window import LogWindow, CustomOutput, gpt3_cmd_handler
 from utils.chat import parse_tf2_console_logs
 from utils.tf2_context import StatsData
+from services.source_game import get_status
+from utils.bot_state import switch_state_hotkey_handler
 
 
 def status_command_sender():
-    from services.source_game import get_status
-    import time
-    while True:
-        get_status()
-        time.sleep(20)
+    with contextlib.suppress(Exception):
+        while True:
+            get_status()
+            time.sleep(20)
 
 
 def get_my_data():
