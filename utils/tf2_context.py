83b07690f16500ccbb97e2582ce81e3a56e8bee9
dborodin836 <dborodin836@gmail.com>
2023-05-31 15:35:27 +0300

    FEAT: added stats about melee crit kills

--- utils/tf2_context.py
+++ utils/tf2_context.py
@@ -8,6 +8,24 @@ from config import config
 from utils.types import Player, SteamHoursApiUrlID64
 from utils.bulk_url_downloader import BulkSteamGameDetailsUrlDownloader
 
+MELEE_WEAPONS_KILL_IDS = {"skullbat", "nonnonviolent_protest", "crossing_guard", "disciplinary_action",
+                          "unique_pickaxe",
+                          "unique_pickaxe_escape", "freedom_staff", "fryingpan", "golden_fryingpan", "demokatana",
+                          "ham_shank", "market_gardener", "memory_maker", "necro_smasher", "paintrain",
+                          "prinny_machete",
+                          "saxxy", "shovel", "bat", "boston_basher", "candy_cane", "warfan", "holymackerel", "sandman",
+                          "lava_bat", "scout_sword", "unarmed_combat", "wrap_assassin", "axtinguisher",
+                          "back_scratcher",
+                          "fireaxe", "sledgehammer", "lollichop", "the_maul", "annihilator", "mailbox", "powerjack",
+                          "lava_axe", "thirddegree", "bottle", "claidheamohmor", "sword", "headtaker", "nessieclub",
+                          "persian_persuader", "batteaxe", "scotland_shard", "voodoo_pin", "eternal_reward"
+                                                                                           "ullapool_caber",
+                          "apocofists", "bread_bite", "eviction_notice", "gloves_running_urgently",
+                          "fists", "steel_fists", "holiday_punch", "gloves", "warrior_spirit", "eureka_effect",
+                          "wrench_golden", "robot_arm", "wrench_jag", "southern_hospitality", "wrench", "amputator",
+                          "bonesaw", "ubersaw", "solemn_vow", "battleneedle", "bushwacka", "club", "shahanshah",
+                          "tribalkukri", "big_earner", "black_rose", "kunai", "knife", "sharp_dresser", "spy_cicle"}
+
 
 def steamid3_to_steamid64(steamid3: str) -> int:
     for ch in ['[', ']']:
@@ -79,18 +97,25 @@ class StatsData:
 
         new_player.steamid64 = steamid3_to_steamid64(new_player.steamid3)
         cls.players.append(new_player)
-        # print(f"Added new player {new_player.name} with {new_player.minutes_on_server} minutes on server")
+        print(f"Added new player {new_player.name} with {new_player.minutes_on_server} minutes on server")
 
     @classmethod
-    def process_kill(cls, killer_username: str, victim_username: str) -> None:
-        print(f"'{killer_username}' killed '{victim_username}'")
+    def process_kill(cls, killer_username: str, victim_username: str, weapon: str, is_crit: bool) -> None:
+        print(f"'{killer_username}' killed '{victim_username} with {weapon} {'crit' if is_crit else ''}'")
+
         for player in cls.players:
             if player.name == killer_username:
+
+                if weapon in MELEE_WEAPONS_KILL_IDS:
+                    player.melee_kills += 1
+                    if is_crit:
+                        player.crit_melee_kills += 1
+
                 player.kills += 1
-                print(f"Incremented kills for a player {player.name}, current value {player.kills}")
+                # print(f"Incremented kills for a player {player.name}, current value {player.kills}")
             elif player.name == victim_username:
                 player.deaths += 1
-                print(f"Incremented deaths for a player {player.name}, current value {player.deaths}")
+                # print(f"Incremented deaths for a player {player.name}, current value {player.deaths}")
 
     @classmethod
     def process_kill_bind(cls, username: str) -> None:
@@ -101,15 +126,23 @@ class StatsData:
                 print(f"Incremented deaths for a player {player.name}, current value {player.deaths}")
 
     @classmethod
-    def _get_steam_prfiles_data(cls) -> List[dict]:
-        steamids64: List[str] = [str(player.steamid64) for player in cls.players if player.steamid64 is not None]
-
+    def _get_steam_profiles_data(cls, steamids64) -> List[dict]:
         try:
             response = requests.get(
                 f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={config.STEAM_WEBAPI_KEY}&steamids={','.join(steamids64)}").json()
             return response["response"]["players"]
+        except Exception:
+            return []
+
+    @classmethod
+    def _get_steam_ban_data(cls, steamids64) -> List[dict]:
+
+        try:
+            response = requests.get(
+                f" http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key={config.STEAM_WEBAPI_KEY}&steamids={','.join(steamids64)}").json()
+            return response["players"]
         except Exception as e:
-            print(e, "123131")
+            print(e)
             return []
 
     @classmethod
@@ -118,10 +151,11 @@ class StatsData:
 
         # Filter players, exclude those who disconnected
         cls.players = [player for player in cls.players if time.time() - int(player.last_updated) < 120]
-        steam_profiles_data_list = cls._get_steam_prfiles_data()
+        id64 = [str(player.steamid64) for player in cls.players if player.steamid64 is not None]
+        steam_profiles_data_list = cls._get_steam_profiles_data(id64)
+        steam_bans_data = cls._get_steam_ban_data(id64)
 
         for player in cls.players:
-            account_age, country, real_name = 'unknown', 'unknown', ""
 
             for player_steam_data in steam_profiles_data_list:
                 if str(player.steamid64) == player_steam_data.get("steamid"):
@@ -144,18 +178,29 @@ class StatsData:
                         },
                         "deaths": player.deaths,
                         "kills": player.kills,
+                        "melee_crit_percentage": cls._get_melee_kill_percentage(player),
                         "k/d": cls.calculate_kd(player),
                         "avg_ping": player.ping,
                         "minutes_on_server": player.minutes_on_server
                     })
 
         new_players = cls._update_tf2_hours(new_players)
+        new_players = cls._update_vac_hours(new_players, steam_bans_data)
 
         return {
             'map': cls.map_name,
             "server_address": cls.server_ip,
             "players": new_players
         }
+
+    @staticmethod
+    def _get_melee_kill_percentage(player: Player) -> str:
+        if player.melee_kills == 0:
+            return "no data"
+
+        percentage = round(player.crit_melee_kills / player.melee_kills * 100, 2)
+
+        return f"Melee crit kill {player.crit_melee_kills}/{player.melee_kills} ({percentage}%)"
 
     @classmethod
     def _update_tf2_hours(cls, to_update_players_list):
@@ -179,6 +224,28 @@ class StatsData:
                             round(response["response"]["games"][0]["playtime_forever"] / 60)) + ' hours'
                     except KeyError:
                         pass
+
+        return to_update_players_list
+
+    @classmethod
+    def _update_vac_hours(cls, to_update_players_list, steam_bans_data):
+        for player_ban_data in steam_bans_data:
+            for player in to_update_players_list:
+                if player["steam"]["steamid64"] == player_ban_data.get("SteamId"):
+                    try:
+                        vac_status = player_ban_data.get("VACBanned", 'unknown')
+                        number_of_bans = player_ban_data.get("NumberOfVACBans", 'unknown')
+                        days_since_last_ban = player_ban_data.get("DaysSinceLastBan", 'unknown')
+
+                        vac = {
+                            "is_VAC_banned": vac_status,
+                            "number_of_bans": number_of_bans,
+                            "days_since_last_ban": days_since_last_ban
+                        }
+
+                        player["steam"]["VAC"] = vac
+                    except KeyError as e:
+                        print(e)
 
         return to_update_players_list
 
