d5e8664a6c96e1a800e166c0e275fe70adc9348e
dborodin836 <dborodin836@gmail.com>
2023-06-11 15:27:57 +0300

    WIP: added one test for epoch time conversion function

---
+++ tests/test_tf_stats.py
@@ -0,0 +1,11 @@
+from utils.tf_statistics import get_date
+
+
+def test_epoch_time():
+    assert get_date(1654005046, 1685541046) == "1 years 0 months 0 days"
+    assert get_date(1527774646, 1685541046) == "5 years 0 months 0 days"
+    assert get_date(1682949743, 1685541046) == "0 years 1 months 0 days"
+    assert get_date(1497448943, 1685541046) == "5 years 11 months 21 days"
+    assert get_date(1331474543, 1685541046) == "11 years 2 months 21 days"
+    assert get_date(1685541046, 1685541046) == "0 years 0 months 0 days"
+
