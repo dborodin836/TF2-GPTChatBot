acd4f4d436de298c32241b04b3c88da0ac90412c
LokiHk <russianpop39@gmail.com>
2024-01-08 00:38:28 +0600

    One click installer
    * FEAT: Installs and runs with a press of a button.
    
    I hope this works.

--- start.bat
+++ start.bat
@@ -1,12 +1,8 @@
 @echo off
-@rem TODO: One-click installer.
-set VENV_DIR=%cd%\venv
-
-if not exist %VENV_DIR% (
-	py -m venv venv || ( echo. && echo Failed to create venv. && goto end )
-	call "venv\scripts\activate"
-	py -m pip install -r requirements.txt || ( echo. && echo Failed to install requirements. && goto end )
+@rem check if venv folder exists, if not, install.
+if exist venv (
+  call "venv/scripts/activate"
+) else (
+  call python one_click.py
 )
-
-call "venv\scripts\activate"
 py main.py
