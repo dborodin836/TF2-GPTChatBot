0ae41085fcbc9402aeef11ab57243d22512ce713
LokiHk <russianpop39@gmail.com>
2024-01-07 21:50:11 +0600

    Create start.bat
    simple bat file to launch the script

---
+++ start.bat
@@ -0,0 +1,15 @@
+@echo off
+@rem TODO: Create venv and pip install -r requirements on initial installation.
+set VENV_DIR=%cd%\venv
+
+if not exist %VENV_DIR% (
+	py -m venv venv || ( echo. && echo Failed to create venv. && goto end )
+	py -m pip install -r requirements.txt || ( echo. && echo Failed to install requirements. && goto end )
+)
+
+if not exist "%VENV_DIR%\pyvenv.cfg" ( echo. && Venv is empty. && goto end )
+
+
+
+call "venv\scripts\activate"
+py main.py
