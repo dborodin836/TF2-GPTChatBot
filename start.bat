0083437902f4b63700289582a536323e3b8c8f2f
LokiHk <russianpop39@gmail.com>
2024-01-09 00:13:52 +0600

    Revert "Merge branch 'master' into ENABLE_SCL"
    This reverts commit f73b0751b091f46ae6ab7fea5de5b70407920eb5, reversing
    changes made to f47cf146fcefa1f1516bfd985cc3155bb249de8f.

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-@rem check if venv folder exists, if not, install.
-set venv = %cd%/venv
-if exist %venv% (
-  call "venv/scripts/activate"
-) else (
-  call python one_click.py
-)
-py main.py
