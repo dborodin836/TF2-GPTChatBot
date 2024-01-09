40d8daf91dfa081aff3fdbfe15339a25530b88ce
Spytech Enjoyer <80442847+teufortressIndustries@users.noreply.github.com>
2024-01-09 09:43:30 +0600

    Update one_click.py

--- one_click.py
+++ one_click.py
@@ -1,18 +1,9 @@
 import sys
 import subprocess
 
-# detect the user's operating system
 os_name = sys.platform
 
-# run the commands depending on the os
-if os_name.startswith("linux"):
-    commands = [
-        "python3 -m venv venv",
-        "source venv/scripts/activate",
-        "pip install -r requirements.txt",
-        "python main.py"
-    ]
-elif os_name.startswith("win"):
+if os_name.startswith("win"):
     commands = [
         "py -m venv venv",
         "call venv/scripts/activate",
@@ -24,5 +15,4 @@ else:
     exit()
 
 # execute the commands in a shell
-for command in commands:
-    subprocess.run(command, shell=True)
+subprocess.run(' & '.join(commands), shell=True)
