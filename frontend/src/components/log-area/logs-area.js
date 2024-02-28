a3c7d827ead650762332063b83ed9a5e263c6d8e
dborodin836 <dborodin836@gmail.com>
2024-02-28 23:55:27 +0200

    FEAT: websocket on frontend implementation

--- frontend/src/components/log-area/logs-area.js
+++ frontend/src/components/log-area/logs-area.js
@@ -1,9 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import { Textarea } from "@material-tailwind/react";
 
 export function LogsArea() {
+
+  const [logs, setLogs] = useState('');
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://127.0.0.1:8000/ws');
+
+    // When message is received, append it to the logs state
+    ws.onmessage = (event) => {
+      setLogs((currentLogs) => `${currentLogs}${event.data}`);
+    };
+
+    // Clean up function to close WebSocket connection when the component unmounts
+    return () => {
+      ws.close();
+    };
+  }, []);
+
   return (
       <div className="flex flex-1 w-full flex-col gap-6 p-4">
-          <Textarea className="min-w-[95%] min-h-[85vh]" size="lg" label="App logs"/>
+          <Textarea
+              className="min-w-[95%] min-h-[85vh]"
+              size="lg"
+              label="App logs"
+              value={logs}
+              readOnly
+          />
       </div>
   );
 }
