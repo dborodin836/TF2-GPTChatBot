b5ff7e15bf46fcd2c52717815031c93aed93cec6
dborodin836 <dborodin836@gmail.com>
2024-03-07 01:08:41 +0200

    add textgen-webui settings section

--- frontend/src/components/page-settings/page-settings.js
+++ frontend/src/components/page-settings/page-settings.js
@@ -63,6 +63,20 @@ export function PageSettings() {
         });
     }
 
+    const toggleCustomModel = () => {
+        setSettings({
+            ...settings,
+            ENABLE_CUSTOM_MODEL: !settings.ENABLE_CUSTOM_MODEL,
+        });
+    }
+
+    const toggleSoftLimitForCustomModel = () => {
+        setSettings({
+            ...settings,
+            ENABLE_SOFT_LIMIT_FOR_CUSTOM_MODEL: !settings.ENABLE_SOFT_LIMIT_FOR_CUSTOM_MODEL,
+        });
+    }
+
     const handleRTDModeChange = (mode) => {
         setSettings((prevSettings) => ({
             ...prevSettings,
@@ -518,6 +532,95 @@ export function PageSettings() {
             </Card>
 
             {/* Textgen Webui */}
+            <Card className="p-6">
+                <Typography className="mb-3" variant="h2">Text Generation WebUI</Typography>
+                <hr className="mb-3"/>
+
+                <div className="ml-1 mb-3">
+                    <Switch label="Enable Text Generation WebUI Integration"
+                            checked={settings?.ENABLE_CUSTOM_MODEL || false}
+                            onChange={toggleCustomModel}/>
+                </div>
+                <hr className="mb-3"/>
+
+                <div className="ml-1 mb-3">
+                    <Switch label="Enable Soft Completion Limit For Custom Model"
+                            checked={settings?.ENABLE_SOFT_LIMIT_FOR_CUSTOM_MODEL || false}
+                            onChange={toggleSoftLimitForCustomModel}/>
+                </div>
+                <hr className="mb-3"/>
+
+                <div className="flex">
+                    <div className="mb-3 w-[100%]">
+                        <label className="mb-2 inline-block text-neutral-500 dark:text-neutral-400">
+                            Custom Model Command
+                        </label>
+                        <div className="w-72 min-w-[100%]">
+                            <Input
+                                className="!border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+                                labelProps={{
+                                    className: "hidden",
+                                }}
+                                containerProps={{className: "min-w-[100px]"}}
+                                value={settings ? settings.CUSTOM_MODEL_COMMAND : ''}
+                                name="CUSTOM_MODEL_COMMAND"
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="mb-3 ml-3 w-[100%]">
+                        <label
+                            className="mb-2 inline-block text-neutral-500 dark:text-neutral-400"
+                        >Custom Model Chat Command</label>
+                        <div className="w-72 min-w-[100%]">
+                            <Input
+                                className="!border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+                                labelProps={{
+                                    className: "hidden",
+                                }}
+                                containerProps={{className: "min-w-[100px]"}}
+                                value={settings ? settings.CUSTOM_MODEL_CHAT_COMMAND : ''}
+                                name="CUSTOM_MODEL_CHAT_COMMAND"
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mb-3">
+                    <label
+                        className="mb-2 inline-block text-neutral-500 dark:text-neutral-400"
+                    >Custom Model Host</label>
+                    <div className="w-72 min-w-[100%]">
+                        <Input
+                            className="!border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+                            labelProps={{
+                                className: "hidden",
+                            }}
+                            containerProps={{className: "min-w-[100px]"}}
+                            value={settings ? settings.CUSTOM_MODEL_HOST : ''}
+                            name="CUSTOM_MODEL_HOST"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                </div>
+
+                <label className="mb-2 inline-block text-neutral-500 dark:text-neutral-400">
+                    Cutom Model Settings
+                </label>
+                <Textarea
+                    className="!border mb-3 !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+                    size="lg"
+                    id="textarea_logs"
+                    labelProps={{
+                        className: "hidden",
+                    }}
+                    value={settings ? settings.CUSTOM_MODEL_SETTINGS : ''}
+                    name="CUSTOM_MODEL_SETTINGS"
+                    onChange={handleInputChange}
+                />
+            </Card>
 
             {/* Stats */}
             <Card className="p-6">
