22798cb16f21cb8219286ec2e1c95c4343902f77
dborodin836 <dborodin836@gmail.com>
2024-03-24 01:25:48 +0200

    convert sidebar to ts, add alt attr to logo

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -8,6 +8,7 @@ import {
 } from "@heroicons/react/24/solid";
 
 import {Link} from 'react-router-dom';
+import React from "react";
 
 export function DefaultSidebar() {
     return (
@@ -16,7 +17,7 @@ export function DefaultSidebar() {
                 className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
                 <nav className="flex flex-col gap-1 w-32 p-2 font-sans text-base font-normal text-gray-700">
                     <Link className="w-32" to="/">
-                        <div role="button" tabIndex="0"
+                        <div role="button"
                              className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-teal-50 hover:bg-opacity-80 active:bg-teal-50 active:bg-opacity-80 hover:text-green-700 active:text-green-700 outline-none">
                             <div className="grid place-items-center mr-4">
                                 <PresentationChartBarIcon className="h-5 w-5"/>
@@ -26,7 +27,7 @@ export function DefaultSidebar() {
                     </Link>
 
                     <Link className="w-32" to="/settings">
-                        <div role="button" tabIndex="0"
+                        <div role="button"
                              className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-teal-50 hover:bg-opacity-80 active:bg-teal-50 active:bg-opacity-80 hover:text-green-700 active:text-green-700 outline-none">
                             <div className="grid place-items-center mr-4">
                                 <Cog6ToothIcon className="h-5 w-5"/>
@@ -36,7 +37,7 @@ export function DefaultSidebar() {
                     </Link>
 
                     <Link className="w-32" to="/about">
-                        <div role="button" tabIndex="0"
+                        <div role="button"
                              className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-teal-50 hover:bg-opacity-80 active:bg-teal-50 active:bg-opacity-80 hover:text-green-700 active:text-green-700 outline-none">
                             <div className="grid place-items-center mr-4">
                                 <UserCircleIcon className="h-5 w-5"/>
