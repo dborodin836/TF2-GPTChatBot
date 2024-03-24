6e272805a29775b959905da4c0bbd485467530ea
dborodin836 <dborodin836@gmail.com>
2024-03-24 13:53:37 +0200

    add comments for logs.py

--- modules/logs.py
+++ modules/logs.py
@@ -1,7 +1,7 @@
 import sys
 from datetime import datetime as dt
 
-from loguru import logger
+from loguru import FilterFunction, Logger, Record, logger
 
 FORMAT_LINE_MAIN = (
     "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: <8} | {name}:{function}:{line} | {message}"
@@ -17,7 +17,27 @@ class LoggerDontExist(Exception):
     ...
 
 
-def get_logger(name: str):
+def get_logger(name: str) -> Logger:
+    """
+    Returns the logger object based on the specified name.
+
+    Main logger:
+        - Logs to a file named log-{time:YYYY-MM-DD}.log in append mode.
+        - Uses DEBUG level logging with a specific format.
+        - Filters messages with 'main' in the name.
+
+    GUI logger:
+        - Logs to stdout with a specific format.
+        - Logs to a file named log-gui-{time:YYYY-MM-DD}.log in append mode.
+        - Uses DEBUG level logging with a specific format.
+        - Filters messages with 'gui' in the name.
+
+    Combo logger:
+        - Logs to stdout with a specific format.
+        - Logs to a file named log-{time:YYYY-MM-DD}.log in append mode.
+        - Uses DEBUG level logging with a specific format.
+        - Filters messages with 'combo' in the name.
+    """
     if name == "main":
         return __main_logger
     elif name == "gui":
@@ -28,14 +48,20 @@ def get_logger(name: str):
         raise LoggerDontExist("Specified logger doesn't exist!")
 
 
-def make_name_filter(name):
-    def filter(record):
+def make_name_filter(name: str) -> FilterFunction:
+    """
+    Create a filter function based on the provided name.
+    """
+    def filter(record: Record) -> bool:
         return record["extra"].get("name") == name
 
     return filter
 
 
 def setup_loggers():
+    """
+    Set up loggers.
+    """
     __main_logger.remove()
     __gui_logger.remove()
     __combo_logger.remove()
@@ -89,5 +115,8 @@ def log_gui_model_message(message_type: str, username: str, user_prompt: str) ->
 
 
 def log_gui_general_message(message: str) -> None:
+    """
+    Logs a general message to the GUI logger with a timestamp.
+    """
     log_msg = f"[{get_time_stamp()}] -- {message}"
     __gui_logger.info(log_msg)
