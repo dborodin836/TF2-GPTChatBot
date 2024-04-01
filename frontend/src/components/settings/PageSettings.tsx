34f93ce3e6947f46b3a4682f8ad54fe1af44c264
dborodin836 <dborodin836@gmail.com>
2024-04-01 19:08:01 +0300

    add Alert messages on field errors

--- frontend/src/components/settings/PageSettings.tsx
+++ frontend/src/components/settings/PageSettings.tsx
@@ -1,142 +1,145 @@
-import React, {useEffect, useState} from "react";
-import {RequireSettings} from "./RequiredSettings";
-import {RCONSettings} from "./RCONSettings";
-import {OpenAISettings} from "./OpenAISettings";
-import {TextgenWebUISettings} from "./TextgenWebUISettings";
-import {StatsSettings} from "./StatsSettings";
-import {RTDSettings} from "./RTDSettings";
-import {MiscellaneousSettings} from "./MiscellaneousSettings";
-import {ExperimentalSettings} from "./ExperimentalSettings";
-import {ChatSettings} from "./ChatSettings";
-import {Controls} from "./Controls";
-import {Settings} from "./SettingsType";
+import React, { useEffect, useState } from "react";
+import { RequireSettings } from "./RequiredSettings";
+import { RCONSettings } from "./RCONSettings";
+import { OpenAISettings } from "./OpenAISettings";
+import { TextgenWebUISettings } from "./TextgenWebUISettings";
+import { StatsSettings } from "./StatsSettings";
+import { RTDSettings } from "./RTDSettings";
+import { MiscellaneousSettings } from "./MiscellaneousSettings";
+import { ExperimentalSettings } from "./ExperimentalSettings";
+import { ChatSettings } from "./ChatSettings";
+import { Controls } from "./Controls";
+import { Settings } from "./SettingsType";
+import { useAlert } from "../AlertContext";
 
+interface ValidationError {
+  ctx: any
+  input: string
+  loc: string[]
+  msg: string
+  type: string
+  url: string
+}
 
 export function PageSettings() {
 
-    const [settings, setSettings] = useState<Settings | null>(null);
+  const [settings, setSettings] = useState<Settings | null>(null);
+  const { openAlert } = useAlert();
 
-    const fetchSettings = async () => {
-        try {
-            const response = await fetch('http://127.0.0.1:8000/settings');
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const data = await response.json();
-            setSettings(data);
-            console.log(data);
-        } catch (error) {
-            console.error("Failed to fetch settings:", error);
-        }
-    };
+  const fetchSettings = async () => {
+    const response = await fetch("http://127.0.0.1:8000/settings");
+    if (!response.ok) {
+      console.error("Failed to fetch settings");
+      openAlert("Failed to fetch settings.");
+    } else {
+      const data = await response.json();
+      setSettings(data);
+    }
+  };
 
-    const fetchDefaultSettings = async () => {
-        try {
-            const response = await fetch('http://127.0.0.1:8000/settings/default');
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const data = await response.json();
-            setSettings(data);
-            console.log(data);
-        } catch (error) {
-            console.error("Failed to fetch default settings:", error);
-        }
-    };
+  const fetchDefaultSettings = async () => {
+    const response = await fetch("http://127.0.0.1:8000/settings/default");
+    if (!response.ok) {
+      console.error("Failed to fetch default settings");
+      openAlert("Failed to fetch default settings.");
+    } else {
+      const data = await response.json();
+      setSettings(data);
+      console.log(data);
+    }
+  };
 
-    useEffect(() => {
-        fetchSettings();
-    }, []);
+  useEffect(() => {
+    fetchSettings();
+  }, []);
 
-    const discardChanges = () => {
-        fetchSettings();
+  const discardChanges = () => {
+    fetchSettings();
+  };
+
+  const submitSettings = async () => {
+    // Ensure there are settings to submit
+    if (!settings) {
+      console.log("No settings to submit");
+      return;
     }
 
-    const submitSettings = async () => {
-        // Ensure there are settings to submit
-        if (!settings) {
-            console.log('No settings to submit');
-            return;
-        }
+    const response = await fetch("http://127.0.0.1:8000/settings", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(settings)
+    });
 
-        try {
-            const response = await fetch('http://127.0.0.1:8000/settings', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(settings),
-            });
+    if (!response.ok) {
+      console.error("Settings are invalid.");
+      let data: ValidationError[] = await response.json()
+      const fieldsWithErrors = data.map((element: ValidationError) => element.loc[0])
+      openAlert(`Settings are invalid. [${fieldsWithErrors.join(', ')}]`);
 
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
-            }
-
-            const result = await response.json();
-            console.log('Successfully submitted settings:', result);
-        } catch (error) {
-            console.error('Failed to submit settings:', error);
-        }
+    } else {
+      openAlert("Successfully saved settings.");
     }
+  };
 
-    const handleRTDModeChange = (mode: number) => {
-        setSettings((prevSettings: any) => ({
-            ...prevSettings,
-            RTD_MODE: mode,
-        }));
-    };
+  const handleRTDModeChange = (mode: number) => {
+    setSettings((prevSettings: any) => ({
+      ...prevSettings,
+      RTD_MODE: mode
+    }));
+  };
 
-    const handleInputChange = (e: any) => {
-        const {name, value} = e.target;
-        setSettings((prevSettings: any) => ({
-            ...prevSettings,
-            [name]: value,
-        }));
-        console.log(e)
-    };
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setSettings((prevSettings: any) => ({
+      ...prevSettings,
+      [name]: value
+    }));
+  };
 
-    const handleToggle = (e: any) => {
-        const {name} = e.target;
-        setSettings((prevSettings: any) => ({
-            ...prevSettings,
-            [name]: !prevSettings[name],
-        }));
-    };
+  const handleToggle = (e: any) => {
+    const { name } = e.target;
+    setSettings((prevSettings: any) => ({
+      ...prevSettings,
+      [name]: !prevSettings[name]
+    }));
+  };
 
 
-    return (
-        <>
-            <Controls submit={submitSettings} discard={discardChanges} getDefaults={fetchDefaultSettings}/>
+  return (
+    <>
+      <Controls submit={submitSettings} discard={discardChanges} getDefaults={fetchDefaultSettings} />
 
-            <div
-                className="flex flex-1 max-h-[calc(100vh-7rem)] flex-col text-gray-700 w-full gap-6 p-4 overflow-y-scroll">
+      <div
+        className="flex flex-1 max-h-[calc(100vh-7rem)] flex-col text-gray-700 w-full gap-6 p-4 overflow-y-scroll">
 
-                <RequireSettings settings={settings} onChangeInput={handleInputChange}/>
+        <RequireSettings settings={settings} onChangeInput={handleInputChange} />
 
-                <RCONSettings settings={settings} onChangeInput={handleInputChange}/>
+        <RCONSettings settings={settings} onChangeInput={handleInputChange} />
 
-                <OpenAISettings settings={settings} onChangeToggle={handleToggle} onChangeInput={handleInputChange}/>
+        <OpenAISettings settings={settings} onChangeToggle={handleToggle} onChangeInput={handleInputChange} />
 
-                <ChatSettings settings={settings} onChangeInput={handleInputChange} onChangeToggle={handleToggle}/>
+        <ChatSettings settings={settings} onChangeInput={handleInputChange} onChangeToggle={handleToggle} />
 
-                <TextgenWebUISettings settings={settings} onChangeToggle={handleToggle}
-                                      onChangeInput={handleInputChange}/>
+        <TextgenWebUISettings settings={settings} onChangeToggle={handleToggle}
+                              onChangeInput={handleInputChange} />
 
-                <StatsSettings settings={settings} onChangeToggle={handleToggle} onChangeInput={handleInputChange}/>
+        <StatsSettings settings={settings} onChangeToggle={handleToggle} onChangeInput={handleInputChange} />
 
-                <RTDSettings settings={settings} onChangeRadio0={() => {
-                    handleRTDModeChange(0)
-                }} onChangeRadio1={() => {
-                    handleRTDModeChange(1)
-                }} onChangeRadio2={() => {
-                    handleRTDModeChange(2)
-                }}/>
+        <RTDSettings settings={settings} onChangeRadio0={() => {
+          handleRTDModeChange(0);
+        }} onChangeRadio1={() => {
+          handleRTDModeChange(1);
+        }} onChangeRadio2={() => {
+          handleRTDModeChange(2);
+        }} />
 
-                <MiscellaneousSettings settings={settings} onChangeToggle={handleToggle}/>
+        <MiscellaneousSettings settings={settings} onChangeToggle={handleToggle} />
 
-                <ExperimentalSettings settings={settings} onChangeToggle={handleToggle}/>
+        <ExperimentalSettings settings={settings} onChangeToggle={handleToggle} />
 
-            </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
