f0429760a197fea9f191aa1ff7d8d62b8250c07c
dborodin836 <dborodin836@gmail.com>
2024-06-23 15:02:16 +0300

    add invoke command

---
+++ modules/commands/gui/invoke.py
@@ -0,0 +1,22 @@
+from config import config
+from modules.chat import controller
+from modules.lobby_manager import lobby_manager
+from modules.logs import get_logger
+from modules.typing import LogLine
+
+gui_logger = get_logger("main")
+
+
+def invoke(command, shared_dict):
+    prompt = command.removeprefix("@").strip()
+    player_lst = list(filter(lambda plr: plr.steamid3 == config.HOST_STEAMID3, lobby_manager.players))
+    if len(player_lst) != 0:
+        player = player_lst[0]
+        logline = LogLine(
+            prompt=prompt,
+            is_team_message=False,
+            username=player.name,
+            player=player
+        )
+        gui_logger.warning(logline)
+        controller.process_line(logline)
--- modules/gui/controller.py
+++ modules/gui/controller.py
@@ -3,6 +3,7 @@ import sys
 from modules.command_controllers import GuiCommandController
 from modules.commands.gui.bans import handle_ban, handle_list_bans, handle_unban
 from modules.commands.gui.config import handle_config
+from modules.commands.gui.invoke import invoke
 from modules.commands.gui.openai import handle_gpt3
 from modules.commands.gui.state import handle_start, handle_stop
 
@@ -12,10 +13,10 @@ config_command_description = """Commands to manipulate config file.
    - get <name> ... - get value(s) from the config
    - save <filename> - write current session config to a file
    - load <filename> - load config from disk
-   - dump - prints whole config to console
-"""
+   - dump - prints whole config to console"""
 
 command_controller = GuiCommandController()
+command_controller.register_command("@", invoke, "Invoke any non-gui command.")
 command_controller.register_command("stop", handle_stop, "Start the bot.")
 command_controller.register_command("start", handle_start, "Stop the bot.")
 command_controller.register_command("ban", handle_ban, "Ban user by username. e.g ban <username>")
