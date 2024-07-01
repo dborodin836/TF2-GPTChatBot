f60bbfa8d8f5f3ba09f47489d4f77b6630808eb9
dborodin836 <dborodin836@gmail.com>
2024-07-01 14:22:26 +0300

    isort fix

--- modules/server/server.py
+++ modules/server/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .routers import commands, settings, root
+from .routers import commands, root, settings
 
 app = FastAPI()
 
